35bf7b1227b7a3add28b3331d28d65a36412ee0c
manoncha3 <mastouche.charlier@gmail.com>
2023-11-13 10:43:29 +0100

    Revert "Update urls.py"
    This reverts commit a4be31e004c3139a0b1ca9d420331cff63f2ca50.

--- AidePourTous/app/urls.py
+++ AidePourTous/app/urls.py
@@ -1,8 +1,10 @@
-pfrom django.urls import path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("home/", views.home, name="home"),
     path("base/", views.base, name="base"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
