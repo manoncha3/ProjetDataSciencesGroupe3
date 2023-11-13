a4be31e004c3139a0b1ca9d420331cff63f2ca50
manoncha3 <mastouche.charlier@gmail.com>
2023-11-13 10:31:10 +0100

    Update urls.py

--- AidePourTous/app/urls.py
+++ AidePourTous/app/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path
+pfrom django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
     path("home/", views.home, name="home"),
     path("base/", views.base, name="base"),
-    path("accounts/", include("django.contrib.auth.urls")),
 ]
