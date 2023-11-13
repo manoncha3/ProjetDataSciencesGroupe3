13e490d1b1b9f7288b9b2b2a79ddcb768e0024bb
BaBaTi07 <baptiste.moncomble@gmail.com>
2023-11-13 02:16:48 +0100

    init django

---
+++ AidePourTous/app/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+
+def base(request):
+    return HttpResponse("You're at the base page.")
+
+def home(request):
+    return HttpResponse("You're at the home page.")
