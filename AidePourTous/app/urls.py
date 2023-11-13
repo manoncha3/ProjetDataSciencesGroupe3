9c6eef5d0c3ba26769537b01ea2bc40f15811f1e
BaBaTi07 <baptiste.moncomble@gmail.com>
2023-11-13 10:17:20 +0100

    git ignore

--- AidePourTous/app/urls.py
+++ AidePourTous/app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("home/", views.home, name="home"),
     path("base/", views.base, name="base"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
